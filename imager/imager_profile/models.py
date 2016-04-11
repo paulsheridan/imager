c449b82e2af8b854149b635ac063af94b75a03a6
paulsheridan <paul.sheridan@me.com>
2016-04-10 18:03:29 -0700

    user model created, not wired quite yet

--- imager/imager_profile/models.py
+++ imager/imager_profile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        qs = super(ActiveManager, self).get_queryset()
+        return qs.filter(user__is_active=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, default='username')
+    email = models.EmailField(default='email')
+    region = models.CharField(default='region')
+    camera = models.CHarField(default='camera')
